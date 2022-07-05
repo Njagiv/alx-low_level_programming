a7e3f8a217c27a63b6f28b32b4911bc09aef503f
Njagiv <nyamburavictory@gmail.com>
2022-07-05 07:20:39 -0700

    First putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - Prints _putchar
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i = 0;
+	char text[] = "_putchar";
 
+	while (text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
