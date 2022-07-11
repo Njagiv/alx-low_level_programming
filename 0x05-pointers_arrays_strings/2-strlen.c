c4cdc1058ac2ca81bdf6591dcc028cc20607da16
Njagiv <nyamburavictory@gmail.com>
2022-07-11 06:38:18 -0700

    Strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Njagiv
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
