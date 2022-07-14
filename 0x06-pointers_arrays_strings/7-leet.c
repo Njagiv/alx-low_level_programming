6948a2909fd7aaa18432fea0aea4a4136f2cbe83
Njagiv <nyamburavictory@gmail.com>
2022-07-14 00:46:43 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -21,11 +21,10 @@ char *leet(char *str)
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
 			if (str[indx1] == leet[indx2] ||
-					str[indx1] - 32 == leet[indx2]
-					str[indx1] = indx2 + '0';
-		}
+				str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0'; }
 
-		index1++;
+		indx1++;
 	}
 
 	return (str);
