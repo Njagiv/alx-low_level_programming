755d9fab68ffe0e84d719baff88a3aea1a37fa29
Njagiv <nyamburavictory@gmail.com>
2022-07-23 08:40:09 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: stores the number of command line arguments
+ * @argv: an array of pointers for all the command line arguments
+ *
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+this is argv and argc
+so fun cant wait
